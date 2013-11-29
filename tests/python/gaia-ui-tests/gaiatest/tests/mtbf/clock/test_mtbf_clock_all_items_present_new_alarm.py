1448bb28383db4a0d0f8b6a54c3fbf9958781968
Brian Huang <brhuang@ubuntu.(none)>
2013-11-29 12:09:49 +0800

    add more cases in clock for mtbf cases.

---
+++ tests/python/gaia-ui-tests/gaiatest/tests/mtbf/clock/test_mtbf_clock_all_items_present_new_alarm.py
@@ -0,0 +1,36 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from gaiatest import GaiaMtbfTestCase
+from gaiatest.apps.clock.app import Clock
+
+
+class TestClockTestAllItemsPresentNewAlarm(GaiaMtbfTestCase):
+
+    def setUp(self):
+        GaiaMtbfTestCase.setUp(self)
+
+        self.clock = Clock(self.marionette)
+        self.app_id = self.launch_by_touch("Clock")
+
+    def test_all_items_present_new_alarm(self):
+        # Wait for the new alarm screen to load
+
+        new_alarm = self.clock.tap_new_alarm()
+
+        # Ensure label has the default placeholder and text
+        self.assertEquals(new_alarm.alarm_label_placeholder, 'Alarm name')
+
+        # Ensure repeat has the default value
+        self.assertEquals(new_alarm.alarm_repeat, 'Never')
+
+        # Ensure sound has the default value
+        self.assertEquals(new_alarm.alarm_sound, 'Classic Buzz')
+
+        # Ensure snooze has the default value
+        self.assertEquals(new_alarm.alarm_snooze, '5 minutes')
+        
+        # Close the new Alarm setting
+        self.marionette.find_element("id", "alarm-close").tap()
+        
