52bc83cb007395c4e1e85c652786bce328b0c6a2
Walter C <wachen@mozilla.com>
2014-01-07 16:09:48 +0800

    modified for new MtbfTestCase class

--- tests/python/gaia-ui-tests/gaiatest/tests/mtbf/fmradio/test_mtbf_fmradio_find_stations.py
+++ tests/python/gaia-ui-tests/gaiatest/tests/mtbf/fmradio/test_mtbf_fmradio_find_stations.py
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from gaiatest import GaiaMtbfTestCase
+from MtbfTestCase import GaiaMtbfTestCase
 from gaiatest.apps.fmradio.app import FmRadio
 
 
