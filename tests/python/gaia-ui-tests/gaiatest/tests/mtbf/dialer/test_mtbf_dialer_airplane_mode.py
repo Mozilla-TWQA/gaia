d68a403a2c4adb1efc8abcb4fc4f13d3dbc2e7a3
Walter C <wachen@mozilla.com>
2014-02-26 17:18:16 +0800

    minor fix

--- tests/python/gaia-ui-tests/gaiatest/tests/mtbf/dialer/test_mtbf_dialer_airplane_mode.py
+++ tests/python/gaia-ui-tests/gaiatest/tests/mtbf/dialer/test_mtbf_dialer_airplane_mode.py
@@ -2,6 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import time
 from MtbfTestCase import GaiaMtbfTestCase
 from gaiatest.apps.phone.app import Phone
 from gaiatest.mtbf_apps.phone.app import MTBF_Phone
